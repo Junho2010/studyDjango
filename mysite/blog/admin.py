dda2b93951ed2da0fc0de6859241088afc589347
junho2010 <554802601@qq.com>
2017-10-20 17:32:47 +0800

    学习提交

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    list_editable = ('title', )
+
+
+admin.site.register(Post, PostAdmin)
